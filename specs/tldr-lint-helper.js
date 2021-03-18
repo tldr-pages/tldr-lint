b94d87538bd80997690326e257b9f1f6c9ee1f59
Matthew Peveler <matt.peveler@gmail.com>
2021-03-18 09:34:37 -0400

    run eslint over specs directory
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- specs/tldr-lint-helper.js
+++ specs/tldr-lint-helper.js
@@ -1,43 +1,46 @@
 var linter = require('../lib/tldr-lint.js');
-var fs = require('fs');
 var path = require('path');
 
-var page_dir = './pages';
-
-lintFile = function(file) {
+var lintFile = function(file) {
   return linter.processFile(path.join(__dirname, file));
 };
 
-containsErrors = function(errors, expected) {
+var containsErrors = function(errors, expected) {
   if (errors.length === 0) return false;
   if (!(expected instanceof Array))
     expected = Array.prototype.splice.call(arguments, 1);
   expected.forEach(function(expectedCode) {
     // If not some correspond to every expected, false
-    if (!errors.some(function(error) { error === expectedCode; }))
+    if (!errors.some(function(error) { return error === expectedCode; }))
       return false;
   });
   return true;
 };
 
-containsOnlyErrors = function(errors, expected) {
+var containsOnlyErrors = function(errors, expected) {
   if (!(expected instanceof Array)) {
     expected = Array.prototype.splice.call(arguments, 1);
   }
   expected.forEach(function(error) {
     if (!containsErrors(errors, error)) {
-      console.error('Couldnt find error', error, 'in these errors')
-      console.error(errors)
+      console.error('Couldnt find error', error, 'in these errors');
+      console.error(errors);
       return false;
-    };
+    }
   });
-  for (var i = 0, error; i < errors.length; i++) {
+  for (var i = 0; i < errors.length; i++) {
     var error = errors[i];
     if (!expected.some(function(expectedCode) {
       return error.code === expectedCode;
     })) {
       return false;
     }
-  };
+  }
   return true;
+};
+
+module.exports = {
+  lintFile,
+  containsErrors,
+  containsOnlyErrors,
 };
