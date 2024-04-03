93f5882483c91e0f0177dde41ddc6017cfca81c0
Juri Dispan <juri.dispan@uni-ulm.de>
2024-04-03 05:42:00 +0200

    feat: TLDR021: Command example should not begin or end in whitespace (#310)
    feat: TLDR021: Command example should not begin or end in whitespace
    
    ---------
    
    Co-authored-by: K.B.Dharun Krishna <kbdharunkrishna@gmail.com>

--- specs/tldr-lint.spec.js
+++ specs/tldr-lint.spec.js
@@ -117,6 +117,12 @@ describe('TLDR conventions', function() {
     expect(containsOnlyErrors(errors, 'TLDR020')).toBeTruthy();
     expect(errors.length).toBe(3);
   });
+
+  it('TLDR021\t' + linter.ERRORS.TLDR021, function() {
+    let errors = lintFile('pages/failing/021.md').errors;
+    expect(containsOnlyErrors(errors, 'TLDR021')).toBeTruthy();
+    expect(errors.length).toBe(2);
+  });
 });
 
 describe('Common TLDR formatting errors', function() {
